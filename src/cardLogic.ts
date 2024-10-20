4a4210a2ef9a2a994a3ae6f4e4263dd4833d9fa3
Olee <olee@Oles-Air.fritz.box>
2024-10-20 12:10:34 +0200

    Implement card drawing and discard logic

---
+++ src/cardLogic.ts
@@ -0,0 +1,67 @@
+import { shuffleArray } from './fishersAlgorithm';
+
+export interface Card {
+	id: string;
+}
+
+export type CardId = Pick<Card, 'id'>;
+
+export interface State {
+	deck: Card[];
+	drawPile: CardId[];
+	discardPile: CardId[];
+	opened: CardId[];
+}
+
+const drawCard = (state: State) => {
+	const [card, ...drawPile] = state.drawPile;
+	return {
+		state: {
+			...state,
+			drawPile
+		},
+		card
+	};
+};
+
+export const openFromDrawPile = (state: State): State => {
+	const { state: newState, card } = drawCard(state);
+	return {
+		...newState,
+		opened: [...state.opened, ...(card ? [card] : [])]
+	};
+};
+
+export const discardFromDrawPile = (state: State): State => {
+	const { state: newState, card } = drawCard(state);
+	return {
+		...newState,
+		discardPile: [...state.discardPile, ...(card ? [card] : [])]
+	};
+};
+
+export const discardFromOpened = (state: State, cardId: CardId): State => {
+	const card = state.opened.find(({ id }) => id === cardId.id);
+	const opened = state.opened.filter(({ id }) => id !== cardId.id);
+	return {
+		...state,
+		opened,
+		discardPile: [...state.discardPile, ...(card ? [card] : [])]
+	};
+};
+
+export const moveFromDiscardToDrawPile = (state: State): State => {
+	return {
+		...state,
+		drawPile: [...state.drawPile, ...state.discardPile],
+		discardPile: []
+	};
+};
+
+export const shuffleDrawPile = (state: State, shuffleMethod?: <T>(array: T[]) => T[]): State => {
+	const drawPile = (shuffleMethod ?? shuffleArray)(state.drawPile);
+	return {
+		...state,
+		drawPile
+	};
+};
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,127 @@
 import { describe, it, expect } from 'vitest';
+import {
+	type Card,
+	discardFromDrawPile,
+	discardFromOpened,
+	moveFromDiscardToDrawPile,
+	openFromDrawPile,
+	shuffleDrawPile,
+	type State
+} from './cardLogic';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+describe('Deck', () => {
+	const deck: Card[] = [{ id: '1' }, { id: '2' }];
+
+	it('should draw first card of drawpile', () => {
+		const state: State = {
+			deck,
+			drawPile: deck.map(({ id }) => ({ id })),
+			discardPile: [],
+			opened: []
+		};
+		expect(openFromDrawPile(state)).toEqual({
+			deck,
+			drawPile: [{ id: '2' }],
+			discardPile: [],
+			opened: [{ id: '1' }]
+		});
+	});
+	it('should change anything if deck is empty', () => {
+		const state: State = {
+			deck: [],
+			drawPile: [],
+			discardPile: [],
+			opened: []
+		};
+		expect(openFromDrawPile(state)).toEqual({
+			deck: [],
+			drawPile: [],
+			discardPile: [],
+			opened: []
+		});
+	});
+	it('should draw last card of drawpile', () => {
+		const state: State = {
+			deck,
+			drawPile: [{ id: '1' }],
+			discardPile: [],
+			opened: []
+		};
+		expect(openFromDrawPile(state)).toEqual({
+			deck,
+			drawPile: [],
+			discardPile: [],
+			opened: [{ id: '1' }]
+		});
+	});
+	it('can discard a card from the drawpile', () => {
+		const state: State = {
+			deck,
+			drawPile: [{ id: '1' }, { id: '2' }],
+			discardPile: [],
+			opened: []
+		};
+		expect(discardFromDrawPile(state)).toEqual({
+			deck,
+			drawPile: [{ id: '2' }],
+			discardPile: [{ id: '1' }],
+			opened: []
+		});
+	});
+	it('can dicard an opened card', () => {
+		const state: State = {
+			deck,
+			drawPile: [],
+			discardPile: [],
+			opened: [{ id: '1' }, { id: '2' }]
+		};
+		expect(discardFromOpened(state, { id: '1' })).toEqual({
+			deck,
+			drawPile: [],
+			discardPile: [{ id: '1' }],
+			opened: [{ id: '2' }]
+		});
+	});
+	it('can move all discard pile to draw pile', () => {
+		const state: State = {
+			deck,
+			drawPile: [],
+			discardPile: [{ id: '1' }, { id: '2' }],
+			opened: []
+		};
+		expect(moveFromDiscardToDrawPile(state)).toEqual({
+			deck,
+			drawPile: [{ id: '1' }, { id: '2' }],
+			discardPile: [],
+			opened: []
+		});
+	});
+	it('can move discard pile and put them on the bottom of the draw pile, keeping the order', () => {
+		const state: State = {
+			deck,
+			drawPile: [{ id: '3' }],
+			discardPile: [{ id: '1' }, { id: '2' }],
+			opened: []
+		};
+		expect(moveFromDiscardToDrawPile(state)).toEqual({
+			deck,
+			drawPile: [{ id: '3' }, { id: '1' }, { id: '2' }],
+			discardPile: [],
+			opened: []
+		});
+	});
+	it("should change the order of the draw pile when it's shuffled", () => {
+		const state: State = {
+			deck,
+			drawPile: [{ id: '1' }, { id: '2' }],
+			discardPile: [],
+			opened: []
+		};
+		expect(shuffleDrawPile(state, <T>(array: T[]) => array.reverse())).toEqual({
+			deck,
+			drawPile: [{ id: '2' }, { id: '1' }],
+			discardPile: [],
+			opened: []
+		});
 	});
 });
